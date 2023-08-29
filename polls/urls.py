275e339550410e3fd728d80cd8bdead73ecb5162
Julia <bjulinhacardoso@gmail.com>
2023-08-29 10:04:34 -0300

    atualizações

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
+# importar da pasta atual o arquivo views.py
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
     path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
-    path('sobre/', views.sobre, name = 'sobre'),
-    path('pergunta/<int:question_id>', views.exibe_questao, name = 'exibe_questao'),
-    path('cadastrar', views.QuestionCreateView.as_view(), name="question-create"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from polls.models import Question
-
 # Create your views here.
+from polls.models import Question, Choice
 
+# View index ... carregada para alguma rota (caminho)
 def index(request):
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'home.html')
-    context = {'titulo':'Página Principal'}
+    # return HttpResponse('Olá... seja bem vindo a enquete')
+    context = {'titulo': 'Página Principal'}
     return render(request, 'home.html', context)
 
 def sobre(request):
@@ -16,19 +15,16 @@ def sobre(request):
 
 def exibe_questao(request, question_id):
     questao = Question.objects.get(id=question_id)
-    # questao.question_text
-    return HttpResponse(questao.question_text)
+    
+    if questao is not None:
+        # questao.question_text
+        return HttpResponse(questao.question_text)
+    
+    return HttpResponse('Não existe questão a exibir')
+
 
 def ultimas_perguntas(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
-   # return render(request, 'polls/perguntas.html', context)
+    # return render(request, 'polls/perguntas.html', context)
     return render(request, 'perguntas_recentes.html', context)
-
-    from django.views.generic.edit import CreateView
-    from django.urls import reverse_lazy
-
-    class QuestionCreateView(CreateView):
-        model = Question
-        fields = ('question_text', 'pub_date')
-        success_url: reverse_lazy('index')
